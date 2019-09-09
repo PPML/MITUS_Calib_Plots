3abd9edcb1746213b4750c8209ccc44e468e5131
nswartwo <nswartwo@gmail.com>
2019-09-09 15:18:25 -0400

    first commit

---
+++ R/calib_plots.R
@@ -0,0 +1,9 @@
+#add in some dependencies
+calib_plots<-function(loc){
+  #list of possible plots
+  #for each of these, these data have to exist in MITUS for this state
+  #list realistic plots(data exist)
+  #for each of these makeadaplota
+  #format these as a data frame
+  #return this dataframe
+}
