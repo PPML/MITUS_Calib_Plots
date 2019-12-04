4f2d34574d2d8c09cd773a3a5ee3e40e2d7f3ab6
Christian Testa <testa101@gmail.com>
2019-12-04 12:49:41 -0500

    Fix bug where calib_plt_us_ltbi_by_age returned the calib_plt_nus_ltbi_by_age function.

--- R/plot_scripts.R
+++ R/plot_scripts.R
@@ -462,6 +462,7 @@ calib_plt_pct_cases_nusb_recent <- function(loc) {
 }
 
 # ----------------------------------------------------------------------------
+#' Plot LTBI Prevalence in Non-US-Born Individuals by Age
 
 calib_plt_us_ltbi_by_age <- function(loc) {
 
@@ -513,65 +514,9 @@ calib_plt_us_ltbi_by_age <- function(loc) {
     #add data source
     labs(caption="target data estimated using National Health and Nutrition Examination Survey (NHANES) data",
          shape = 'target')
-
-  # ----------------------------------------------------------------------------
-
-  #' Plot LTBI Prevalence in Non-US-Born Individuals by Age
-
-  calib_plt_nus_ltbi_by_age <- function(loc) {
-    #set the location
-    loc1<-loc
-    if (loc1 != "US") {loc2<-"ST"} else {loc2<-loc1}
-    #read in the target data
-    #find file name
-    fn<-list.files(pattern="prev_NUSB_11_IGRA",system.file(paste0(loc2,"/calibration_targets/"),package = "MITUS"))
-    #subset into 2006-2016
-    target_df0 <-readRDS(system.file(paste0(loc2,"/calibration_targets/",fn),package="MITUS"))  #read in the model output data
-    #sum across years
-    target_df<-as.data.frame(target_df0[,2]/rowSums(target_df0[,2:3])*100)
-    #update the column names for legend use
-
-    label<-c("5-14 years","15-24 years ",
-             "25-34 years","35-44 years","45-54 years",
-             "55-64 years","65-74 years","75+ years")
-    rownames(target_df)<-label
-    target_df<-as.data.frame(target_df); colnames(target_df)<-"percentage"
-    #read in the model output data
-    #find file name
-    fn<-list.files(pattern = "NUSB_LTBI_pct", system.file(paste0(loc1, "/calibration_outputs/"), package="MITUS"))
-    outcomes_df0 <-readRDS(system.file(paste0(loc1,"/calibration_outputs/",fn), package="MITUS"))
-    outcomes_df<-as.data.frame(outcomes_df0)
-    rownames(outcomes_df)<-label;colnames(outcomes_df)<-"percentage"
-    #calculate the error
-    error_ltbi<-readRDS(system.file("US/error_ltbi.rds", package="MITUS"))[[1]]
-    error_df <- cbind.data.frame(label = label, error_ltbi, target_df)
-    colnames(error_df)[2:4] <- c('lower', 'upper', 'target')
-
-    #set up the plot options
-    ggplot() + theme_bw() + ylab("") +xlab("Age Group")+ theme(legend.position="bottom") +
-      scale_x_discrete(limits=label)+
-      #add the model output
-      geom_col(data=outcomes_df, aes(x=label, y=percentage, fill="dodgerblue2"),alpha=0.5) +
-      #add the target data
-      # geom_point(data=target_df, aes(x=label, y=percentage*100, color="black" ) ) +
-      #add legend
-      scale_color_manual("", values="black", label="LTBI % target")+
-      scale_fill_manual("", values="dodgerblue2", label="LTBI % model output")+
-      #add in the error bars
-      # geom_segment(aes(x=label, y=x[,1], xend=label, yend=x[,2]))+
-      geom_pointrange(data = error_df, mapping = aes(x = label, y = target, ymax = upper, ymin = lower, color = 'black')) +
-      # geom_pointrange(data=outcomes_df,aes(x=label, y=percentage, ymin=x[,1], ymax=x[,2]))+
-      #add plot title
-      ggtitle(paste0("IGRA+ LTBI in Non-US Born Population 2011 in ",loc," by Age (%) [NATIONAL]"))+
-      #add data source
-      labs(caption="target data estimated using National Health and Nutrition Examination Survey (NHANES) data",
-           shape = 'target')
-  }
-
-  # ----------------------------------------------------------------------------
-
-
 }
+
+
 
 # ----------------------------------------------------------------------------
 #' Plot LTBI Prevalence in Non-US-Born Individuals by Age
