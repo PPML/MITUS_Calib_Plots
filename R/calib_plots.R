c50144bed65ccce1bd3a596b2e48e85c83cc1221
Christian Testa <testa101@gmail.com>
2019-12-04 13:00:40 -0500

    Suggested Edit: Add optional parameter plot_subset to calib_plots

--- R/calib_plots.R
+++ R/calib_plots.R
@@ -5,15 +5,26 @@
 #' plots are provided description in the other columns. 
 #' 
 #' @param loc A two-letter code for the location 
+#' @param plots_subset (optional) a subset of shortnames to use to select which
+#' plots are computed and not included in the return value plots. 
+#' @return A tibble with 6 columns: category, shortname, name, plot, target_data, output_data
 #' @export
-calib_plots<-function(loc){
-  #create a dataframe to hold plots and accompanying data
+calib_plots<-function(loc, plots_subset = NULL){
+
+  # create a dataframe to hold plots and accompanying data
   possible_plots<-all_possible_plots_df(loc)
-  #check that the data exists for all the plots
+
+  # check that the data exists for all the plots
   plots<-data_check(loc, possible_plots) #returns list of all available plots short name
+
+  # filter the plots data frame for shortname == plots_subset to only compute a
+  # subset if not plots all are needed
+  if (! is.null(plots_subset)) plots <- plots[plots$shortname %in% plots_subset, ]
+
   #make the plots
   for (i in 1:nrow(plots)){
     plots[[i,4]]<-make_plots(loc,as.character(plots[i,2]))
   }
+
   return(plots)
 }
